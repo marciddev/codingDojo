d277b3f48683afd2286e7267021ab6d9e9a45442
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-09-30 16:39:27 -0500

    init

--- Python Stack/Pytho/fundamentals/for_loop_basic1.py
+++ Python Stack/Pytho/fundamentals/for_loop_basic1.py
@@ -1,26 +1,34 @@
 #basic
-for i in range(0, 151):
-    print(i)
-for i in range(5, 1001, 5):
-    print(i)
-for i in range(1, 100):
-    if i % 10 == 0:
-        print('coding dojo')
-    elif i % 5 == 0:
-        print('coding')
-    else:
-        print(i)
-for i in range(0, 500000):
-    sum = 0
-    sum = sum + i
-print(sum)
-for i in range(2018, -1, -4):
-    print(i)
-lowNum = 3
-highNum = 350
-mult = 5
-for i in range(lowNum, highNum+1):
-    if(i % mult == 0):
-        print(i)
-    else:
-        continue
+# for i in range(0, 151):
+#     print(i)
+# for i in range(5, 1001, 5):
+#     print(i)
+# for i in range(1, 100):
+#     if i % 10 == 0:
+#         print('coding dojo')
+#     elif i % 5 == 0:
+#         print('coding')
+#     else:
+#         print(i)
+# for i in range(0, 500000):
+#     sum = 0
+#     sum = sum + i
+# print(sum)
+# for i in range(2018, -1, -4):
+#     print(i)
+# lowNum = 3
+# highNum = 350
+# mult = 5
+# for i in range(lowNum, highNum+1):
+#     if(i % mult == 0):
+#         print(i)
+#     else:
+#         continue
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+print(b)
+a()
+print(b)
---
+++ Python Stack/Pytho/fundamentals/functionsBasic2.py
@@ -0,0 +1,27 @@
+# def countDown(num):
+#     myListing = []
+#     for x in range(num, -1,-1):
+#         myListing.append(x)
+#     return myListing
+# print(countDown(10))
+# def printAndReturn(a, b):
+#     newList = [a, b]
+#     print(a)
+#     return b
+# def firstPlusLength(li):
+#     sum = 0
+#     sum = li[0] + len(li)
+#     return sum
+# def valueGreaterThanSecond(li):
+#     newList = []
+#     for x in li:
+#         if x != li[0] and x != li[1] and x > li[1]:
+#             newList.append(x)
+#     return newList
+# print(valueGreaterThanSecond([1, 3, 5, 1, 9]))
+# def thisLenThatVal(size, val):
+#     newLi = []
+#     for x in range(0, size):
+#         newLi.append(val)
+#     return newLi
+# print(thisLenThatVal(5, 7))
