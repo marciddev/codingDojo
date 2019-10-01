1f8938e4f64c9a441c63785d9a0a1aceeb8eac4e
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-10-01 13:34:44 -0500

    updated for chaining

--- Python Stack/Pytho/oop/users.py
+++ Python Stack/Pytho/oop/users.py
@@ -4,31 +4,24 @@ class User:
         self.user_balance = 0
     def make_deposit(self, amount):
         self.user_balance += amount
+        return self
     def make_withdrawl(self,amount):
         self.user_balance -= amount
+        return self
     def display_user_balance(self):
         print(self.user_balance)
+        return self
     def transfer_money(self, amount, user):
         self.user_balance -= amount
         user.user_balance += amount
         print(self.name + ' has transferred ' + str(amount) + ' to ' + user.name)
+        return self
 user2 = User('kevin')
 user3 = User('andrew')
 user4 = User('amber')
-user2.make_deposit(15)
-user2.make_deposit(15)
-user2.make_deposit(15)
-user2.make_withdrawl(5)
-user2.display_user_balance()
-user3.make_deposit(10)
-user3.make_deposit(15)
-user3.make_withdrawl(5)
-user3.make_withdrawl(3)
-user3.display_user_balance()
-user4.make_deposit(500)
-user4.make_withdrawl(5)
-user4.make_withdrawl(10)
-user4.make_withdrawl(15)
+user2.make_deposit(15).make_deposit(15).make_deposit(15).make_withdrawl(5).display_user_balance()
+user3.make_deposit(10).make_deposit(15).make_withdrawl(5).make_withdrawl(3).display_user_balance()
+user4.make_deposit(500).make_withdrawl(5).make_withdrawl(10).make_withdrawl(15).display_user_balance()
 user2.display_user_balance()
 user4.display_user_balance()
 user2.transfer_money(35, user4)
