e2e329bdfa415d53cb42baa884c27af9f2104858
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-10-03 15:32:25 -0500

    init

--- Python Stack/flask/flask_fundamentals/hello flask/hello.py
+++ Python Stack/flask/flask_fundamentals/hello flask/hello.py
@@ -12,5 +12,14 @@ def color(times, color):
 @app.route('/success')
 def success():
     return "success"  
+@app.route('/lists')
+def render_lists():
+    student_info = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    return render_template("lists.html", students = student_info)
 if __name__=="__main__":  
     app.run(debug=True)    
