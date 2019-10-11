4f07adc9c7e203536aa5fc2654238c5ccb406bc8
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-10-11 15:43:18 -0500

    Added validations

--- Python Stack/django/django_full_stack/semirestfultvshows/apps/Main/models.py
+++ Python Stack/django/django_full_stack/semirestfultvshows/apps/Main/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ShowManager(models.Manager):
+    def validate(self, postData):
+        errors = {}
+        if len(postData['showtitle']) < 1:
+            errors['showtitle'] = "Title must be more than 1 letter"
+        if len(postData['networkname']) < 1:
+            errors['networkname'] = "Network name must be more than 1 letter"
+        if len(postData['showdesc']) < 1:
+            errors['showdesc'] = "Description must be more than 1 letter"
+        return errors
 class Show(models.Model):
     title=models.CharField(max_length=255)
     network=models.CharField(max_length=10)
@@ -8,5 +18,6 @@ class Show(models.Model):
     description=models.TextField()
     updated_at=models.DateTimeField(auto_now=True)
     created_at=models.DateTimeField(auto_now_add=True)
+    objects = ShowManager()
     def __repr__(self):
         return f"ID: {self.id} Title: {self.title} Network: {self.network} Release Date: {self.release_date}"
--- Python Stack/django/django_full_stack/semirestfultvshows/apps/Main/views.py
+++ Python Stack/django/django_full_stack/semirestfultvshows/apps/Main/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, HttpResponse, render, redirect
 from apps.Main.models import *
+from django.contrib import messages
 # Create your views here.
 def homepage(request):
     return redirect("/shows")
 def newshows(request):#GET METHOD: Shows the create a show form...
     return render(request, "Main/newshowstemplate.html")
 def createshow(request):#POST METHOD: Gathers the new show data and adds to db and redirects to /shows/<id>
-    if request.method == "POST":
-        if request.POST["hide"] == "createshow":
-            n=Show.objects.create(title=request.POST['showtitle'],network=request.POST['networkname'],release_date=request.POST['showdate'],description=request.POST['showdesc'])
-            n.save()
-    return redirect(f"/shows/{n.id}")
+    errors=Show.objects.validate(request.POST)
+    if len(errors) > 0:
+        for k,v in errors.items():
+            messages.error(request, v)
+        return redirect(f"/shows/new")
+    else:
+        if request.method == "POST":
+            if request.POST["hide"] == "createshow":
+                n=Show.objects.create(title=request.POST['showtitle'],network=request.POST['networkname'],release_date=request.POST['showdate'],description=request.POST['showdesc'])
+                n.save()
+        return redirect(f"/shows/{n.id}")
+
 def viewshow(request, grabid):#GET METHOD: Showing the individual show's "profile"
     request.session['ident'] = grabid
     context = {
@@ -30,22 +38,36 @@ def showstable(request):#GET METHOD: Rendering the template with the data table
     return render(request, "Main/tabletemplate.html", context)
 def editshow(request, grabid2):#GET METHOD: Rendering the template with the edit show feature.
     context = {
-        "iden" : grabid2
+        "iden" : grabid2,
+        "title": Show.objects.get(id=grabid2).title,
+        "network": Show.objects.get(id=grabid2).network,
+        "date": Show.objects.get(id=grabid2).release_date,
+        "description": Show.objects.get(id=grabid2).description
     }
     return render(request, "Main/editshows.html", context)
 def updateshow(request, grabid3):#POST METHOD: Udpating the information they input to the appropriate show id.
-    if request.method == "POST":
-        if request.POST['hide'] == "editshow":
-            n2= Show.objects.get(id=grabid3)
-            n2.title=request.POST['editshowtitle']
-            n2.network=request.POST['editnetworkname']
-            n2.release_date=request.POST['editshowdate']
-            n2.description=request.POST['editshowdesc']
-            n2.save()
-    context = {
-        "thirdid": grabid3
-    }
-    return redirect(f"/shows/{n2.id}", context)#POST METHOD: Deletes the show
+    errors=Show.objects.validate(request.POST)
+    if len(errors) > 0:
+        na = Show.objects.get(id=grabid3).id
+        for k,v in errors.items():
+            messages.error(request, v)
+        context = {
+            "thirdid": grabid3
+        }
+        return redirect(f"/shows/{grabid3}/edit", context)
+    else: 
+        if request.method == "POST":
+            if request.POST['hide'] == "editshow":
+                n2= Show.objects.get(id=grabid3)
+                n2.title=request.POST['editshowtitle']
+                n2.network=request.POST['editnetworkname']
+                n2.release_date=request.POST['editshowdate']
+                n2.description=request.POST['editshowdesc']
+                n2.save()
+        context = {
+            "thirdid": grabid3
+        }
+        return redirect(f"/shows/{n2.id}", context)#POST METHOD: Deletes the show
 def deleteshow(request, grabid4):
     if request.method == "POST":
         if request.POST['hide'] == "destroy2":
