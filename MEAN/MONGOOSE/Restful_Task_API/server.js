dcdaa35bd8688111262996b109bc4fa9984df829
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-11-07 14:15:35 -0600

    added first angular assignment

--- MEAN/MONGOOSE/Restful_Task_API/server.js
+++ MEAN/MONGOOSE/Restful_Task_API/server.js
@@ -5,6 +5,7 @@ app.set('views', __dirname + '/client/views');
 app.set('view engine', 'ejs');
 
 app.use(express.json());
+app.use(express.static(__dirname + '/public/dist/public'));
 
 
 require('./server/config/mongoose').main();
