f21414fb3eea2a686ebfc922e36c1563f50d27d7
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-11-07 15:57:24 -0600

    added restful tasks assignment

--- MEAN/MONGOOSE/Restful_Task_API/public/src/app/app.module.ts
+++ MEAN/MONGOOSE/Restful_Task_API/public/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpService } from './http.service';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
