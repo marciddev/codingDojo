3ff32b07e93a03801a1c5cddc63b28850800d28d
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-11-08 13:20:26 -0600

    added assignment

--- MEAN/MONGOOSE/Restful_Task_API/public/src/app/app.component.ts
+++ MEAN/MONGOOSE/Restful_Task_API/public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service'
 
 @Component({
@@ -6,9 +6,23 @@ import { HttpService } from './http.service'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'public';
+export class AppComponent implements OnInit {
+  title : String;
+  tasks = [];
   constructor(private _httpService: HttpService) {
     
+  }
+  ngOnInit() {
+    this.getTasksFromService();
+    this.title = "hello";
+  }
+  getTasksFromService() {
+    let observe = this._httpService.getTasks();
+    observe.subscribe(data => {
+      console.log("got our tasks,!!! ", data);
+      for(var x in data) {
+        this.tasks.push({title: data[x].title, description: data[x].description})
+      }
+    })
   }
 }
--- MEAN/MONGOOSE/Restful_Task_API/public/src/app/http.service.ts
+++ MEAN/MONGOOSE/Restful_Task_API/public/src/app/http.service.ts
@@ -6,18 +6,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HttpService {
   constructor(private _http: HttpClient) { 
-    this.getTasks();
-    this.getOneTask("5dc1ff6abd199d37801b2e24");
   }
   getTasks() {
-    let tempObservable = this._http.get('/tasks');
-    // subscribe to the Observable and provide the code we would like to do with our data from the response
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
-  }
-  getOneTask(id) {
-    let tempObservable = this._http.get(`/tasks/${id}`);
-    tempObservable.subscribe(data => {
-      console.log(data);
-    })
+    return this._http.get('/tasks');
   }
 }
