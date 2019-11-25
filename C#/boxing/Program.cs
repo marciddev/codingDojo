edd5b5ac44fd9cdb5ac21e8d243d5f657d28fa9c
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-11-25 13:22:06 -0600

    added boxing assignment

---
+++ C#/boxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> listable = new List<object>();
+            listable.Add(7);
+            listable.Add(28);
+            listable.Add(true);
+            listable.Add("chair");
+            listable.Add(-1);
+            int sum = 0;
+            foreach(var i in listable) { 
+                Console.WriteLine(i);
+                if(i is int) {
+                    sum += (int)i;
+                }
+            }
+                Console.WriteLine(sum);
+
+        }
+    }
+}
