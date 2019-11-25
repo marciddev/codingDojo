8f12dcffe42c416cc8bec496abb82f71f646842b
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-11-25 12:37:09 -0600

    added optional assignment

---
+++ C#/table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] table = new int[10,10];
+
+            for(int i=1;i<11;i++) {
+                for(int j=1;j<11;j++) {
+                    table[i-1,j-1] = i * j;
+                    Console.WriteLine(table[i-1,j-1]);
+                }
+            }
+        }
+    }
+}
