0c9b26681dce9310f59890f2be84ce95434c5f6c
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-11-26 12:28:40 -0600

    Added deck of cards and singly linked lists assignments

---
+++ C#/deckofcards/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace deckofcards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck d = new Deck();
+            Player p = new Player("Kevin");
+            foreach(Card c in d.cards) {
+                Console.WriteLine($"{c.stringVal} {c.suit}");
+            }
+            d.Shuffle();
+            Console.WriteLine("**************************************");
+            foreach(Card c in d.cards) {
+                Console.WriteLine($"{c.stringVal} {c.suit}");
+            }
+            Console.WriteLine("*****************************************");
+            d.Reset();
+            foreach(Card c in d.cards) {
+                Console.WriteLine($"{c.stringVal} {c.suit}");
+            }
+            p.Draw(d);
+            foreach(Card c in p.Hand) {
+                Console.WriteLine($"{c.stringVal} {c.suit} ");
+            }
+            foreach(Card c in d.cards) {
+                Console.WriteLine($"{c.stringVal} {c.suit}");
+            }
+        }
+    }
+    class Card {
+        public string stringVal;
+        public string suit;
+        public int val;
+    }
+    class Deck {
+        public List<Card> cards = new List<Card>();
+        public Deck() {
+            for(int i=0;i<4;i++) {
+                for(int j=0;j<13;j++) {
+                    Card card = new Card();
+                    if(j == 0) {
+                        card.stringVal = "Ace";
+                    } else if(j == 10) {
+                        card.stringVal = "Jack";
+                    } else if(j == 11) {
+                        card.stringVal = "Queen";
+                    } else if(j == 12) {
+                        card.stringVal = "King";
+                    } else {
+                        card.stringVal = $"{j}";
+                    }
+                    card.val = j;
+                    if(i == 0) {
+                        card.suit = "Clubs";
+                    } else if(i == 1) {
+                        card.suit = "Spades";
+                    } else if(i == 2) {
+                        card.suit = "Hearts";
+                    } else if(i == 3) {
+                        card.suit = "Diamonds";
+                    }
+                    cards.Add(card);
+
+                }
+            }
+        }
+        public Card Deal() {
+            Card c = cards[cards.Count-1];
+            cards.RemoveAt(cards.Count-1);
+            return c;
+        }
+        public void Reset() {
+            cards.Clear();
+            for(int i=0;i<4;i++) {
+                for(int j=0;j<13;j++) {
+                    Card card = new Card();
+                    if(j == 0) {
+                        card.stringVal = "Ace";
+                    } else if(j == 10) {
+                        card.stringVal = "Jack";
+                    } else if(j == 11) {
+                        card.stringVal = "Queen";
+                    } else if(j == 12) {
+                        card.stringVal = "King";
+                    } else {
+                        card.stringVal = $"{j}";
+                    }
+                    card.val = j;
+                    if(i == 0) {
+                        card.suit = "Clubs";
+                    } else if(i == 1) {
+                        card.suit = "Spades";
+                    } else if(i == 2) {
+                        card.suit = "Hearts";
+                    } else if(i == 3) {
+                        card.suit = "Diamonds";
+                    }
+                    cards.Add(card);
+
+                }
+            }
+        }
+        public List<Card> Shuffle() {
+            Random r = new Random();
+            for(int i=0;i<cards.Count;i++) {
+                int next = (int)r.Next(0,52);
+                Card temp = cards[i];
+                cards[i] = cards[next];
+                cards[next] = temp;
+            }
+            return cards;
+        }
+    }
+    class Player {
+        public string Name;
+        public List<Card> Hand = new List<Card>();
+        public Player(string n) {
+            Name = n;
+        }
+        public Card Draw(Deck d) {
+            Card drawn = d.Deal();
+            Hand.Add(drawn);
+            return drawn;
+        }
+        public Card Discard(int index) {
+            if(Hand[index] == null) {
+                return null;
+            } else {
+                return Hand[index];
+            }
+        }
+    }
+}
---
+++ C#/sll/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace sll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SLNode node1 = new SLNode(6);
+            SLNode node2 = new SLNode(3);
+            SLNode node3 = new SLNode(5);
+            SLL list = new SLL();
+            list.addFront(6);
+            node1.next = node2;
+            node2.next = node3;
+            foreach(int i in list.display()) {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(list.Length());
+            
+        }
+    }
+    class SLNode {
+        public SLNode next;
+        public int value;
+        public SLNode(int val) {
+            next = null;
+            value = val;
+        }
+    }
+    class SLL {
+        public SLNode head;
+        public SLL() {
+            SLNode head = null;
+        }
+        public void addFront(int val) {
+            SLNode node = new SLNode(val);
+            node.next = head;
+            head = node;
+        }
+        public SLNode removeFront() {
+            head = head.next;
+            return head;
+        }
+        public bool contains(int val) {
+            SLNode runner = head;
+            while(runner != null) {
+                if(runner.value == val) {
+                    return true;
+                }
+                runner = runner.next;
+            }
+            return false;
+        }
+        public int Length() {
+            int count = 0;
+            SLNode runner = head;
+            while(runner != null) {
+                count++;
+                runner = runner.next;
+            }
+            return count;
+        }
+        public int[] display() {
+            Console.WriteLine(Length());
+            int[] num = new int[Length()];
+            SLNode runner = head;
+            int count = 0;
+            while (runner != null) {
+                num[count] = runner.value;
+                runner = runner.next;
+                count++;
+            }
+            return num;
+        }
+
+    }
+}
