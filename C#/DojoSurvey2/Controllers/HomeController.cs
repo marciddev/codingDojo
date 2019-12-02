4127e4e511fa45399eaca215619c15131b49f594
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-12-02 14:46:55 -0600

    Added Dojo Survey With Models

---
+++ C#/DojoSurvey2/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey2.Models;
+
+namespace DojoSurvey2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("/submit")]
+        public IActionResult Submit(Survey s) {
+            return View(s);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ C#/DojoSurvey2/Models/Survey.cs
@@ -0,0 +1,8 @@
+namespace DojoSurvey2.Models {
+    public class Survey {
+        public string name {get;set;}
+        public string location {get;set;}
+        public string language {get;set;}
+        public string comment {get;set;}
+    }
+}
