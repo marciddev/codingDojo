b66ce0cbce6a496a81ca6abbb91cd8e3ce875659
kevin.kvc34 <kevin.kvc34@gmail.com>
2019-12-03 11:29:54 -0600

    Added random passcode

---
+++ C#/randompass/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using randompass.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace randompass.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string generatePasscode(int i) {
+            Random r = new Random();
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+            string s = "";
+            for(int j=0;j<=i;j++) {
+                s += chars[r.Next(0, chars.Length)];
+            }
+            return s;
+        }
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            if(HttpContext.Session.GetInt32("times") == null) {
+                HttpContext.Session.SetInt32("times", 0);
+            }
+
+            ViewBag.Passcode = HttpContext.Session.GetString("passcode");
+            ViewBag.Times = HttpContext.Session.GetInt32("times");
+
+            return View();
+            
+        }
+        [HttpGet("/genPass")]
+        public IActionResult genPass() {
+            Console.WriteLine(HttpContext.Session.GetInt32("times") + "*****");
+            int? currCount = HttpContext.Session.GetInt32("times");
+            currCount++;
+            HttpContext.Session.SetInt32("times", (int)currCount);
+            HttpContext.Session.SetString("passcode", generatePasscode(14));
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ C#/randompass/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace randompass.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
